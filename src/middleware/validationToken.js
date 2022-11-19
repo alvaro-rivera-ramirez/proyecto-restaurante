82ba900a7590f2911fc3afad4c07ff37a2e7ed08
jcondorio <78916447+jcondorio@users.noreply.github.com>
2022-11-19 10:12:26 -0500

    arreglando

--- src/middleware/validationToken.js
+++ src/middleware/validationToken.js
@@ -1,15 +1,20 @@
 const {verifyToken} = require("../utils/handleToken");
 const {handleErrorResponse,handleHttpError}=require("../utils/handleError")
 const validationToken = async (req, res, next) => {
+  const { jwt } = req.cookies;
+  if (!jwt){
+    handleErrorResponse(res,"NO_CREDENTIALS",401)
+    return;
+  }
   try {
 
-=======
-    const { jwt } = req.cookies;
-    if (!jwt){
+    const token = jwt.split(" ")[1];
+
+    if (!token){
       handleErrorResponse(res,"NO_CREDENTIALS",401)
       return;
     }
-    
+
     const { payload } = await verifyToken(token);
     req.id = payload.user.id_usu;
 
