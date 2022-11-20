973e214ff39ed8da62a07897f1c4ad5b77ba2e23
jcondorio <78916447+jcondorio@users.noreply.github.com>
2022-11-19 19:40:12 -0500

    con detalles v1.0

--- src/routes/interface/indexInterface.js
+++ src/routes/interface/indexInterface.js
@@ -30,12 +30,12 @@ router.get('/change-password',isLoggedIn,async(req,res)=>{
 //Interfaz registro usuarios
 router.get('/register-user',isLoggedIn,async(req,res)=>{
     const {nom_usu,nom_tipousu}= await getProfileHeader(req.id);
-    res.render('registerUser',{nom_usu,nom_tipousu});
+    res.render('admin/registerUser',{nom_usu,nom_tipousu});
 })
 //Interfaz vista registro usuarios
 router.get('/Users',isLoggedIn,async(req,res)=>{
     const {nom_usu,nom_tipousu}= await getProfileHeader(req.id);
-    res.render('adminUsers',{nom_usu,nom_tipousu});
+    res.render('admin/adminUsers',{nom_usu,nom_tipousu});
 })
 
 module.exports=router;
