4dc3ecb331e1fcf1bfbba757a8c7f71701b161b9
YinoSoncco <ysonccop@unjbg.edu.pe>
2022-12-14 19:40:49 -0500

    primer avance

--- src/routes/interface/meseroInterface.js
+++ src/routes/interface/meseroInterface.js
@@ -11,5 +11,9 @@ router.get('/realizar-pedido',isLoggedIn,(req,res)=>{
     const nom_tipousu=req.role
     res.render('mesero/realizar-pedido',{nom_usu,nom_tipousu});
 })
-
+router.get('/cliente',isLoggedIn,(req,res)=>{
+  const nom_usu=req.name
+  const nom_tipousu=req.role
+  res.render('mesero/cliente',{nom_usu,nom_tipousu});
+})
 module.exports=router
