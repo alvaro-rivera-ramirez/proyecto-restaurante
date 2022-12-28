f113c4be032bd74ebcf5f7a4b6b9f87dcd20c972
jcondorio <78916447+jcondorio@users.noreply.github.com>
2022-12-28 13:22:57 -0500

    aa

--- src/routes/interface/adminInterface.js
+++ src/routes/interface/adminInterface.js
@@ -47,4 +47,9 @@ router.get("/NuevoProducto", isLoggedIn,(req, res) => {
   const nom_tipousu=req.role
   res.render("admin/newproducto",{nom_usu,nom_tipousu});
 });
+router.get("/estadisticasusu", isLoggedIn,(req, res) => {
+  const nom_usu=req.name
+  const nom_tipousu=req.role
+  res.render("admin/adminStatsUsu",{nom_usu,nom_tipousu});
+});
 module.exports=router
