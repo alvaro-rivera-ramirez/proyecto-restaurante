61a87a3a75ce42c822df5c0e2629d325d84b544e
alvarorivera <alvarorivera2001@gmail.com>
2023-01-16 08:02:25 -0500

    validacion de crear cliente y buscar

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const {validationToken}=require("../middleware/validationToken")
+const {validateCreateClient,validateSearchClient}=require("../middleware/validators/client")
 const ClientController=require("../controllers/client.controller");
 const ClientRouter=Router();
 
-ClientRouter.get("/:dni",ClientController.getClientByDni);
-ClientRouter.post("/",ClientController.createClient);
+ClientRouter.get("/:dni",validationToken,validateSearchClient,ClientController.getClientByDni);
+ClientRouter.post("/",validationToken,validateCreateClient,ClientController.createClient);
 
 module.exports=ClientRouter;
